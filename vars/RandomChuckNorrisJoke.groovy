aa2210af03e46288746822e86decb316c1d2fd41
Brian G. Shacklett <brian@digital-traffic.net>
2019-04-21 13:39:14 -0400

    add simple step with external dep

---
+++ vars/RandomChuckNorrisJoke.groovy
@@ -0,0 +1,18 @@
+import com.example.SharedLibraryConstants
+import org.apache.httpcomponents.httpclient  
+
+def call() {
+  def jsonSlurper = new JsonSlurper()
+  def httpclient  = HttpClients.createDefault();
+  def httpGet     = new HttpGet('http://api.icndb.com/jokes/random');
+  def response    = httpclient.execute(httpGet);
+
+
+  try {
+    def jokeResp = jsonSlurper.parseText(response.getEntity().getContent())
+  } finally {
+    response.close();
+  }
+
+  return jokeResp.value.joke
+}
